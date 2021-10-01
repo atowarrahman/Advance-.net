78d3cc4b84f702498e39306d84a9ef8986c44df2
Atowar Rahman <emonatowarrahman@gmail.com.com>
2021-10-01 09:25:47 +0600

    first commit

---
+++ CV/CV/Controllers/personController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CV.Controllers
+{
+    public class personController : Controller
+    {
+     public ActionResult Bio()
+        {
+            return View();
+        }
+        public ActionResult education()
+        {
+            return View();
+        }
+        public ActionResult personalInfo()
+        {
+            return View();
+        }
+        public ActionResult projects()
+        {
+            return View();
+        }
+        public ActionResult references()
+        {
+            return View();
+        }
+    }
+}
